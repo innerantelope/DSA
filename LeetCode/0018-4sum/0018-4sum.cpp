cffce26ec30058b564f85a36c1d9f3bb80a6f9e7
Ayush Jha <jhaa009971@gmail.com>
2025-07-27 10:50:54 +0530

    Time: 23 ms (57.16%), Space: 17.5 MB (29.55%) - LeetArchive

---
+++ LeetCode/0018-4sum/0018-4sum.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        vector<vector<int>> result;
+        sort(nums.begin(), nums.end());
+        for (int i = 0; i < nums.size(); i++) {
+            if (i > 0 && nums[i] == nums[i - 1])
+                continue;
+            for (int j = i + 1; j < nums.size(); j++) {
+                if (j > i + 1 && nums[j] == nums[j - 1])
+                    continue;
+                int k = j + 1;
+                int l = nums.size() - 1;
+                while (k < l) {
+                    long long sum = 1LL * nums[i] + nums[j] + nums[k] + nums[l];
+                    if (sum == target) {
+                        result.push_back({nums[i], nums[j], nums[k], nums[l]});
+                        while (k < l && nums[k] == nums[k +1]) k++;
+                        while (k < l && nums[l] == nums[l - 1]) l--;
+                        k++;
+                        l--;
+                    }
+                    else if (sum < target) {
+                        k++;
+                    } else
+                        l--;
+                }
+            }
+        }
+        return result;
+    }
+};
