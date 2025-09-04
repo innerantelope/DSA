0c9106a41abdc0e2bfc259620c9a770d6d4d6e97
Ayush Jha <jhaa009971@gmail.com>
2025-09-04 11:24:44 +0530

    Time: 0 ms (100%), Space: 8.6 MB (55.11%) - LeetArchive

---
+++ LeetCode/3830-find-closest-person/3830-find-closest-person.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    int findClosest(int x, int y, int z) {
+        if(abs(y-z)>abs(x-z)) return 1;
+        else if(abs(x-z)>abs(y-z)) return 2;
+        else return 0;
+    }
+};
