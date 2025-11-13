90446c2deb42acebd05980db882095117ef74fce
Ayush Jha <jhaa009971@gmail.com>
2025-11-13 19:16:14 +0530

    Time: 3 ms (83.33%), Space: 15.8 MB (53.99%) - LeetArchive

---
+++ LeetCode/3493-maximum-number-of-operations-to-move-ones-to-the-end/3493-maximum-number-of-operations-to-move-ones-to-the-end.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int maxOperations(string s) {
+        int op =0;
+        int ones = 0;
+        for (int i =0;i < s.size(); i++){
+            if(s[i]=='1') ones++;
+            else if (i>0 && s[i-1]=='1') op+=ones;
+        }
+        return op;
+        
+    }
+};
