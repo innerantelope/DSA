cb4e20318aa169bdeffe038d1b18ba5b944ca4d2
Ayush Jha <jhaa009971@gmail.com>
2026-02-06 18:13:48 +0530

    Time: 36 ms (41.57%), Space: 105.2 MB (14.24%) - LeetArchive

---
+++ LeetCode/3958-minimum-removals-to-balance-array/3958-minimum-removals-to-balance-array.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int minRemoval(vector<int>& nums, int k) {
+         int n = nums.size();
+        
+        sort(nums.begin(), nums.end());
+        
+        int maxi = 1;
+        int left = 0;
+        
+        for (int right = 0; right < n; right++) {
+            while (left <= right && (long long)nums[right] > (long long)k * nums[left]) {
+                left++;
+            }
+            
+            if (left <= right) {
+                maxi = max(maxi, right - left + 1);
+            }
+        }
+        
+        return n - maxi;
+        
+    }
+};
