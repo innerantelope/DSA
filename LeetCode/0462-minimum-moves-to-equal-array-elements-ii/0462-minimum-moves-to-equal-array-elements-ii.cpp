fd8027b54eb1b2ea18b7923c58e5e75f035271a9
Ayush Jha <jhaa009971@gmail.com>
2026-04-20 21:09:19 +0530

    Time: 0 ms (100%), Space: 15.7 MB (51.69%) - LeetArchive

---
+++ LeetCode/0462-minimum-moves-to-equal-array-elements-ii/0462-minimum-moves-to-equal-array-elements-ii.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int minMoves2(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        int median=nums[nums.size()/2];
+        int cost=0;
+        for(int i = 0; i < nums.size(); i++) {
+        cost += abs(nums[i] - median);
+        }
+        return cost;
+        
+    }
+};
