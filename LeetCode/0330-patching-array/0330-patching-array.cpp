e0452e8cb8ea6b4071c5ec70e4142f033ceef157
Ayush Jha <jhaa009971@gmail.com>
2026-04-21 12:20:32 +0530

    Time: 0 ms (100%), Space: 15 MB (48.11%) - LeetArchive

---
+++ LeetCode/0330-patching-array/0330-patching-array.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int minPatches(vector<int>& nums, int n) {
+        long long miss=1;
+        int i=0,patch=0;
+        while(miss<=n){
+            if(i<nums.size() && nums[i]<=miss){
+                miss+=nums[i];
+                i++;
+
+            }
+            else{
+                miss+=miss;
+                patch++;
+            }
+        }
+
+        return patch;
+        
+    }
+};
