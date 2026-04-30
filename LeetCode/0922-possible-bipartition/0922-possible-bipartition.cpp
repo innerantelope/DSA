6b5717eeda1c89bb8e25712c40b516c022c97884
Ayush Jha <jhaa009971@gmail.com>
2026-04-30 12:52:07 +0530

    Time: 34 ms (64.17%), Space: 69.2 MB (60.69%) - LeetArchive

---
+++ LeetCode/0922-possible-bipartition/0922-possible-bipartition.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    bool possibleBipartition(int n, vector<vector<int>>& dislikes) {
+    vector<vector<int>> adj(n + 1);
+    for(auto &d : dislikes) {
+        adj[d[0]].push_back(d[1]);
+        adj[d[1]].push_back(d[0]);
+    }
+    vector<int> color(n + 1, -1);
+    for(int i=1;i<=n;i++){
+       if(color[i]==-1){
+        queue<int>q;
+        q.push(i);
+        color[i]=0;
+        while(!q.empty()){
+            int node=q.front();
+            q.pop();
+            for(int neig:adj[node]){
+                if(color[neig]==-1){
+                     color[neig]=1-color[node];
+                      q.push(neig);
+                }
+                else if(color[node]==color[neig]) return false;
+            }
+        }
+       }
+
+    }
+    return true;
+
+        
+    }
+};
