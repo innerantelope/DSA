2423a74d2bfde39ea83b8529777cad3efdb1d057
Ayush Jha <jhaa009971@gmail.com>
2026-05-31 11:22:35 +0530

    Time: 30 ms (69.63%), Space: 106.7 MB (81.61%) - LeetArchive

---
+++ LeetCode/2245-destroying-asteroids/2245-destroying-asteroids.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool asteroidsDestroyed(int mass, vector<int>& asteroids) {
+        sort(asteroids.begin(),asteroids.end());
+        long long mass1=mass;
+        for(int i=0;i<asteroids.size();i++){
+            if(mass1>=asteroids[i]) mass1+=asteroids[i];
+            else return false;
+        }
+        return true;
+    }
+};
