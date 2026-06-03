f94cae853fb3bba3bf858b8d062f7599a1af50e9
Ayush Jha <jhaa009971@gmail.com>
2026-06-03 21:05:30 +0530

    Time: 4 ms (73.39%), Space: 238 MB (66.06%) - LeetArchive

---
+++ LeetCode/3967-earliest-finish-time-for-land-and-water-rides-ii/3967-earliest-finish-time-for-land-and-water-rides-ii.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int earliestFinishTime(vector<int>& landStartTime, vector<int>& landDuration, vector<int>& waterStartTime, vector<int>& waterDuration) {
+        int n=landDuration.size();
+        int m=waterStartTime.size();
+        int ans=INT_MAX;
+        int earliestland=INT_MAX;
+        for(int i=0;i<n;i++){
+            if(landStartTime[i]+landDuration[i]<earliestland) 
+                earliestland=landStartTime[i]+landDuration[i];
+        }
+        int newtime=earliestland;
+        for(int i=0;i<m;i++){
+            int final=earliestland+max(waterStartTime[i]-newtime,0)+waterDuration[i];
+            ans=min(ans,final);
+        }
+        int earliestwater=INT_MAX;
+        for(int i=0;i<m;i++){
+            if(waterStartTime[i]+waterDuration[i]<earliestwater) 
+                earliestwater=waterStartTime[i]+waterDuration[i];
+        }
+        newtime=earliestwater;
+        for(int i=0;i<n;i++){
+            int final=earliestwater+max(landStartTime[i]-newtime,0)+landDuration[i];
+            ans=min(ans,final);
+        }
+        return ans;
+    }
+};
