02f0f75a0114b35970777420a91a90aeb3a90bdb
Ayush Jha <jhaa009971@gmail.com>
2026-06-23 22:26:23 +0530

    Time: 416 ms (58.54%), Space: 105.2 MB (58.86%) - LeetArchive

---
+++ LeetCode/3628-find-minimum-time-to-reach-last-room-ii/3628-find-minimum-time-to-reach-last-room-ii.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int minTimeToReach(vector<vector<int>>& moveTime) {
+          //{time,r,c}
+        int m=moveTime.size();
+        int n=moveTime[0].size();
+        vector<vector<int>>dis(m,vector<int>(n,INT_MAX));
+        dis[0][0]=0;
+        priority_queue<tuple<int,int,int,int>,vector<tuple<int,int,int,int>>,greater<>>pq;
+        int p=0;
+        pq.push({0,0,0,0});
+        vector<vector<int>>dir={{0,-1},{0,1},{-1,0},{1,0}};
+        while(!pq.empty()){
+            auto[t,r,c,s]=pq.top();
+            pq.pop();
+            if(t>dis[r][c]) continue;
+            for(int k=0;k<4;k++){
+                int nr=r+dir[k][0];
+                int nc=c+dir[k][1];
+                if(nr>=0 && nc>=0 && nr<m && nc<n){
+                    int mc=(s==0?1:2);
+                    int nt=max(t,moveTime[nr][nc])+mc;
+                    if(nt<dis[nr][nc]){
+                        dis[nr][nc]=nt;
+                        p=s^1;
+                        pq.push({nt,nr,nc,p});
+                    }
+                }
+
+            }
+
+
+        }
+
+        return dis[m-1][n-1];
+
+
+        
+    }
+};
