dac71e4c21d5487b3fafdc8965a4c854dd7724e6
Ayush Jha <jhaa009971@gmail.com>
2026-06-29 21:09:27 +0530

    Time: 0 ms (100%), Space: 11.6 MB (61.65%) - LeetArchive

---
+++ LeetCode/2099-number-of-strings-that-appear-as-substrings-in-word/2099-number-of-strings-that-appear-as-substrings-in-word.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int numOfStrings(vector<string>& patterns, string word) {
+        int count=0;
+        for(string &pat:patterns){
+            if(word.find(pat)!=string::npos){
+                count++;
+            }
+        }
+        return count;
+    }
+};
