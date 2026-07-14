2f4799ced76e02f3343615ff13acd1cff0dbecab
Ayush Jha <jhaa009971@gmail.com>
2026-07-14 14:36:56 +0530

    Time: 218 ms (18.79%), Space: 13.6 MB (47.11%) - LeetArchive

---
+++ LeetCode/0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int t[1001][1001];
+    bool solve(int i,int j,string&s){
+        if(i>=j) return 1;
+        if(t[i][j]!=-1) return t[i][j];
+        if(s[i]==s[j]) return t[i][j]=solve(i+1,j-1,s);
+        return t[i][j]=0;
+    }
+    string longestPalindrome(string s) {
+        int n=s.size();
+        memset(t,-1,sizeof(t));
+        int maxl=INT_MIN;
+        int p=0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(solve(i,j,s)){
+                    if(j-i+1>maxl){
+                        maxl=j-i+1;
+                        p=i;
+                    }
+
+                }
+            }
+        }
+        return s.substr(p,maxl);
+        
+    }
+};
